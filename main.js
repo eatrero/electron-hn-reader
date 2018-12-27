099a5ea26b45b58262c87aa07cb82f76e3fbfbc9
Ed Atrero <eatrero@gmail.com>
2018-12-27 12:22:24 -0800

    upgrade babel

--- main.js
+++ main.js
@@ -1,9 +1,7 @@
-"use strict";
-
 // Import parts of electron to use
-const { app, BrowserWindow } = require("electron");
-const path = require("path");
-const url = require("url");
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+const url = require('url');
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
@@ -22,9 +20,9 @@ if (
 
 // Temporary fix broken high-dpi scale factor on Windows (125% scaling)
 // info: https://github.com/electron/electron/issues/9691
-if (process.platform === "win32") {
-  app.commandLine.appendSwitch("high-dpi-support", "true");
-  app.commandLine.appendSwitch("force-device-scale-factor", "1");
+if (process.platform === 'win32') {
+  app.commandLine.appendSwitch('high-dpi-support', 'true');
+  app.commandLine.appendSwitch('force-device-scale-factor', '1');
 }
 
 function createWindow() {
@@ -39,17 +37,17 @@ function createWindow() {
   let indexPath;
 
   // Implementing Webpack
-  if (dev && process.argv.indexOf("--noDevServer") === -1) {
+  if (dev && process.argv.indexOf('--noDevServer') === -1) {
     indexPath = url.format({
-      protocol: "http:",
-      host: "localhost:8080",
-      pathname: "index.html",
+      protocol: 'http:',
+      host: 'localhost:8080',
+      pathname: 'index.html',
       slashes: true
     });
   } else {
     indexPath = url.format({
-      protocol: "file:",
-      pathname: path.join(__dirname, "dist", "index.html"),
+      protocol: 'file:',
+      pathname: path.join(__dirname, 'dist', 'index.html'),
       slashes: true
     });
   }
@@ -57,17 +55,17 @@ function createWindow() {
   mainWindow.loadURL(indexPath);
 
   // Don't show until we are ready and loaded
-  mainWindow.once("ready-to-show", () => {
+  mainWindow.once('ready-to-show', () => {
     mainWindow.show();
 
     // Open the DevTools automatically if developing
     if (dev) {
-      mainWindow.webContents.openDevTools();
+      // mainWindow.webContents.openDevTools();
     }
   });
 
   // Emitted when the window is closed.
-  mainWindow.on("closed", function() {
+  mainWindow.on('closed', function() {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
@@ -78,18 +76,18 @@ function createWindow() {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on("ready", createWindow);
+app.on('ready', createWindow);
 
 // Quit when all windows are closed.
-app.on("window-all-closed", () => {
+app.on('window-all-closed', () => {
   // On macOS it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== "darwin") {
+  if (process.platform !== 'darwin') {
     app.quit();
   }
 });
 
-app.on("activate", () => {
+app.on('activate', () => {
   // On macOS it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (mainWindow === null) {
--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
-import React from "react";
-import { render } from "react-dom";
+import React from 'react';
+import { render } from 'react-dom';
+import styled from 'styled-components';
 
+const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: palevioletred;
+`;
 // Create main App component
 class App extends React.Component {
   render() {
     return (
       <div>
-        <h1>Hello, this is your first Electron app!</h1>
+        <Title>Hello, this is your first Electron app!</Title>
 
         <p>I hope you enjoy using this electron react app. Woot</p>
       </div>
@@ -15,11 +21,11 @@ class App extends React.Component {
 }
 
 // Create your own root div in the body element before rendering into it
-let root = document.createElement("div");
+let root = document.createElement('div');
 
 // Add id 'root' and append the div to body element
-root.id = "root";
+root.id = 'root';
 document.body.appendChild(root);
 
 // Render the application into the DOM, the div inside index.html
-render(<App />, document.getElementById("root"));
+render(<App />, document.getElementById('root'));
